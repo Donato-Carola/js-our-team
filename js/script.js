31f5be74803667cc377a0819af8853b528edc056
Donato-Carola <don94@live.it>
2023-11-24 16:11:04 +0100

    stampa a schermo usando il dom le varie Card

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 /* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 MILESTONE 0:
@@ -14,5 +12,71 @@ BONUS 2:
 Organizzare i singoli membri in card/schede, potete usare come esempio lo screenshot,  
 ma anche realizzare qualcosa di creativo!!!*/
 
+/*
+Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg*/
+
+//? costuire l'array di oggetti  */
+
+const team = [
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    foto: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    foto: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    foto: 'wayne-barnett-founder-ceo.jpg',
+  },
+];
+
+console.log(team);
 
 
+ //
+const mainParent = document.querySelector("section");
+  const sectionInfo = document.querySelector("main>section>article");
+for (let i = 0; i < team.length; i++) {
+    
+  sectionInfo.innerHTML += 
+  `
+  <div class="card widthCard" >
+  <img src="${team[i].foto}" class="card-img-top" alt="${team[i].nome}">
+  <div class="card-body text-center">
+    <h5 class="card-title">${team[i].nome}</h5>
+    <p class="card-text">${team[i].ruolo}</p>
+    
+  </div>
+</div>
+  
+    `
+  mainParent.appendChild(sectionInfo);
+}
+ /*<img src="${team[i].foto}" alt="${team[i].nome}"></img>
+    <p>${team[i].nome}</p>
+    <p>${team[i].ruolo}</p>*/
